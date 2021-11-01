82183fcd935c62729ed81c9fca9de16f9307bd78
Adrienne <adriennepaquin@gmail.com>
2021-10-31 20:37:00 -0400

    dropdowns for filter

--- client/src/components/Workouts.js
+++ client/src/components/Workouts.js
@@ -2,6 +2,7 @@ import DisplayWorkout from './DisplayWorkout'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 function Workouts( {user, workouts} ) {
 
@@ -14,6 +15,45 @@ function Workouts( {user, workouts} ) {
         <Container>
             <Row>
                 <h2>All Workouts</h2>
+                <h3>Filter Workouts By:</h3>
+                <Dropdown>
+                    <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                        Length
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item >5</Dropdown.Item>
+                        <Dropdown.Item >10</Dropdown.Item>
+                        <Dropdown.Item >15</Dropdown.Item>
+                        <Dropdown.Item >20</Dropdown.Item>
+                        <Dropdown.Item >30</Dropdown.Item>
+                        <Dropdown.Item >45</Dropdown.Item>
+                        <Dropdown.Item >60</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown>
+                    <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                        Class Type
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item >Cycling</Dropdown.Item>
+                        <Dropdown.Item >Yoga</Dropdown.Item>
+                        <Dropdown.Item >Strength</Dropdown.Item>
+                        <Dropdown.Item >Meditation</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown>
+                    <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                        Instructor
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item >Alex</Dropdown.Item>
+                        <Dropdown.Item >Anna</Dropdown.Item>
+                        <Dropdown.Item >Christine</Dropdown.Item>
+                        <Dropdown.Item >Cody</Dropdown.Item>
+                        <Dropdown.Item >Jess</Dropdown.Item>
+
+                    </Dropdown.Menu>
+                </Dropdown>
             </Row>
             <Row xs={1} md={2} className="g-4">
                 {displayWorkouts}
